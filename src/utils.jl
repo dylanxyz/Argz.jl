857f825c9d0fbb5275152f7c3fb3e53d0b65f366
Jorge Brito <jorge.brito.xyz@gmail.com>
2022-06-15 22:19:38 -0300

    Implement custom exceptions for parsing error

--- src/utils.jl
+++ src/utils.jl
@@ -1,2 +1,22 @@
 spaces(n::Integer) = repeat(" ", n)
 ssplit(vec, c) = string.(strip.(split(vec, c)))
+isoption(s::String) = occursin(opt_arg_re, s)
+
+struct InvalidOption <: Exception
+    option::String
+end
+
+struct MissingOptValue <: Exception
+    option::String
+end
+
+function Base.showerror(io::IO, e::InvalidOption)
+    print(io, "InvalidOption: ")
+    print(io, e.option)
+end
+
+function Base.showerror(io::IO, e::MissingOptValue)
+    print(io, "MissingOptValue: ")
+    print(io, "missing value for option ")
+    print(io, e.option)
+end
